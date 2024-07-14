097fa9c9e71d9778b6c41a3aa7293f2d39584c73
syrmon <mmehemmsd@gmail.com>
2024-07-14 21:06:10 +0400

    categories design

--- components/sections/Categories.js
+++ components/sections/Categories.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
-import { FreeMode, Pagination } from "swiper/modules";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
 
 const Categories = ({ currentIndex }) => {
   return (
-    <div className="themesflat-container">
+    <>
       <div className="cat-container desktop-hider">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          freeMode={true}
-          pagination={{
-            clickable: true,
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 2,
+            drag: "free",
+            gap: "1rem",
+            pagination: false,
+            arrows: false,
           }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
         >
-          <SwiperSlide className="swiper-slide-box">
+          <SplideSlide>
             <a href="/car-list">
               <button
                 className={`index-slider-items ${
@@ -31,121 +28,131 @@ const Categories = ({ currentIndex }) => {
                 <img src="/assets/images/categories/n3.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="truck75">
                 <label htmlFor="truck75">Truck</label>
                 <img src="/assets/images/categories/n1.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="truck">
                 <label htmlFor="truck">Semi-trailer truck</label>
                 <img src="/assets/images/categories/n2.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
+            <a href="/car-list">
+              <button className="index-slider-items" id="truck75">
+                <label htmlFor="truck75">Truck</label>
+                <img src="/assets/images/categories/n1.png" alt="" />
+              </button>
+            </a>
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="cm">
                 <label htmlFor="cm">Construction machinery</label>
                 <img src="/assets/images/categories/n4.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="av">
                 <label htmlFor="av">Agricultural vehicle</label>
                 <img src="/assets/images/categories/n5.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="bus">
                 <label htmlFor="bus">Bus</label>
                 <img src="/assets/images/categories/n6.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="forklift">
                 <label htmlFor="forklift">Forklift</label>
                 <img src="/assets/images/categories/n7.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide-box">
+          </SplideSlide>
+          <SplideSlide>
             <a href="/car-list">
               <button className="index-slider-items" id="sp">
                 <label htmlFor="sp">Spare parts</label>
                 <img src="/assets/images/categories/n8.png" alt="" />
               </button>
             </a>
-          </SwiperSlide>
-        </Swiper>
+          </SplideSlide>
+        </Splide>
       </div>
-      <div className="cat-container mobile-hider ">
-        <a href="/car-list">
-          <button
-            className={`index-slider-items ${
-              currentIndex === true ? "active" : ""
-            }`}
-            id="semi-trailer"
-          >
-            <label htmlFor="semi-trailer">Semi-trailer</label>
-            <img src="/assets/images/categories/n3.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="truck75">
-            <label htmlFor="truck75">Truck</label>
-            <img src="/assets/images/categories/n1.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="truck">
-            <label htmlFor="truck">Semi-trailer truck</label>
-            <img src="/assets/images/categories/n2.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="cm">
-            <label htmlFor="cm">Construction machinery</label>
-            <img src="/assets/images/categories/n4.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="av">
-            <label htmlFor="av">Agricultural vehicle</label>
-            <img src="/assets/images/categories/n5.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="bus">
-            <label htmlFor="bus">Bus</label>
-            <img src="/assets/images/categories/n6.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="forklift">
-            <label htmlFor="forklift">Forklift</label>
-            <img src="/assets/images/categories/n7.png" alt="" />
-          </button>
-        </a>
-        <a href="/car-list">
-          <button className="index-slider-items" id="sp">
-            <label htmlFor="sp">Spare parts</label>
-            <img src="/assets/images/categories/n8.png" alt="" />
-          </button>
-        </a>
+      <div className="themesflat-container">
+        <div className="cat-container mobile-hider ">
+          <a href="/car-list">
+            <button
+              className={`index-slider-items ${
+                currentIndex === true ? "active" : ""
+              }`}
+              id="semi-trailer"
+            >
+              <label htmlFor="semi-trailer">Semi-trailer</label>
+              <img src="/assets/images/categories/n3.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="truck75">
+              <label htmlFor="truck75">Truck</label>
+              <img src="/assets/images/categories/n1.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="truck">
+              <label htmlFor="truck">Semi-trailer truck</label>
+              <img src="/assets/images/categories/n2.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="cm">
+              <label htmlFor="cm">Construction machinery</label>
+              <img src="/assets/images/categories/n4.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="av">
+              <label htmlFor="av">Agricultural vehicle</label>
+              <img src="/assets/images/categories/n5.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="bus">
+              <label htmlFor="bus">Bus</label>
+              <img src="/assets/images/categories/n6.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="forklift">
+              <label htmlFor="forklift">Forklift</label>
+              <img src="/assets/images/categories/n7.png" alt="" />
+            </button>
+          </a>
+          <a href="/car-list">
+            <button className="index-slider-items" id="sp">
+              <label htmlFor="sp">Spare parts</label>
+              <img src="/assets/images/categories/n8.png" alt="" />
+            </button>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 

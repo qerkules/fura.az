86d111c41d829e40528f0597647d4f08afd1365f
syrmon <mmehemmsd@gmail.com>
2024-07-22 20:11:35 +0400

    up

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "https://furaapi.aifdigital.com.tr/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },

2bbf713e7454ecac067e2fe6c3d82b4c35a42078
syrmon <mmehemmsd@gmail.com>
2024-08-02 01:10:02 +0400

    craeted

---
+++ app/details/[slug]/page.js
@@ -0,0 +1,248 @@
+"use client";
+import Layout from "@/components/layout/Layout";
+import ThumbSlider from "@/components/slider/ThumbSlider";
+import Link from "next/link";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import AdCard from "@/components/layout/AdCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const description =
+  " There are many variations of passages of Lorem Ipsum available, but majority have suffered teration in some form, by injected humour, or randomised words which don't look even slight believable. If you are going to use a passa There are many variations of passages of Lorem Ipsum available, but majority have suffered teration in some form look even by injected humour, or randomised There are many variations of passages of Lorem Ipsum available, but majority have suffered teration in some form, by injected humour, or randomised words which don't look even slight believable. If you are going to use a passa There are many variations of passages of Lorem Ipsum available, but majority have suffered teration in some form, by injected humour, or randomised many variations of passages of Lorem Ipsum available, but majority There are many variations of passages of";
+
+const overviewArray = [
+  { title: "Year", value: "2020" },
+  { title: "Permissible Weight", value: "7.5T" },
+  { title: "Distance", value: "200.000 km" },
+  { title: "Engine Power", value: "500 hp" },
+  { title: "FuelType", value: "Benzin" },
+  { title: "Paint", value: "Red" },
+  { title: "Retarder", value: "Not Available" },
+  { title: "Intrader", value: "Not Available" },
+  { title: "Damaged", value: "No Damage" },
+  { title: "Gearbox", value: "Manual" },
+  { title: "Cylinder Volume", value: "1500cc" },
+  { title: "Wheel Formula", value: "4x4" },
+  { title: "Driving Cabin", value: "Long Road" },
+  { title: "Steering", value: "Left" },
+  { title: "Axles", value: "2" },
+  { title: "Air Conditioning", value: "Automatic" },
+  { title: "Emission Class", value: "Euro 6" },
+  { title: "Emission Sticker", value: "1 (None)" },
+];
+const featuresArray1 = [
+  { value: "2020" },
+  { value: "Alloy wheels" },
+  { value: "Metallic color" },
+  { value: "Exterior trim" },
+  { value: "ABS" },
+];
+
+export default function ListingDetails({ params }) {
+  const inputString = params.slug;
+  const [product, setProduct] = useState({});
+  const [relatedAds, setRelatedAds] = useState([]);
+
+  const decodedString = decodeURIComponent(inputString);
+
+  const parts = decodedString.split("|");
+
+  const adPath = parts[0];
+
+  const adId = parts[1];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_LINK}/${adPath}/GetByAdId?adId=${adId}`
+        );
+        setProduct(data?.data || {});
+
+        const datas = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_LINK}/${adPath}/GetAll${adPath}Ads?CurrentPage=1&PageSize=8`
+        );
+        setRelatedAds(datas?.data?.trucksList?.$values || []);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Layout headerStyle={1} footerStyle={1}>
+        <div>
+          <div className="widget-breakcrumb">
+            <div className="themesflat-container">
+              <div className="breakcrumb">
+                <div className="title-breakcrumb">
+                  <Link className="home" href="/">
+                    Home
+                  </Link>
+                  <span>Semi Trailer Truck</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="widget-property-detail">
+            <div className="themesflat-container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="wrap-property-car flex">
+                    <div className="box-1">
+                      <div className="text-address">
+                        <i className="icon-map-1-1" />
+                        <p>Baku, Azerbaijan</p>
+                      </div>
+                      <div className="title-heading">
+                        {product.brandName} {product.modelName}{" "}
+                      </div>
+                      <div className="icon-box-info flex">
+                        <div className="info flex">
+                          <span>Category:</span>
+                          <span className="fw-4">{product.categoryName}</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="box-2">
+                      <div className="icon-boxs flex">
+                        <Link href="/#">
+                          <i className="icon-heart-1-1" />
+                          <span>Favorite</span>
+                        </Link>
+                        <Link href="/#">
+                          <i className="icon-shuffle-2-11" />
+                          <span>Compare</span>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-8">
+                  <div className="gallary-property-details">
+                    <ThumbSlider images={product.images?.$values} />
+                  </div>
+                  <div className="post-property">
+                    <div className="wrap-car-overview wrap-style">
+                      <h4 className="title">Car Overview</h4>
+                      <div className="listing-info">
+                        <div className="row">
+                          {overviewArray.map((data, index) => (
+                            <div key={index} className="col-xl-6 col-md-6 item">
+                              <div className="inner listing-infor-box">
+                                <div className="content-listing-info">
+                                  <span className="listing-info-title">
+                                    {data.title}
+                                  </span>
+                                  <p className="listing-info-value">
+                                    {data.value}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="wrap-description wrap-style">
+                      <h4 className="title">Description</h4>
+                      <p>{product.adDetails}</p>
+                    </div>
+                    <div className="wrap-car-feature wrap-style">
+                      <h4 className="title">Features</h4>
+                      <div className="tf-listing-info">
+                        <div id="tf-features">
+                          {featuresArray1.map((data, index) => {
+                            return (
+                              <div key={index} className="listing-feature-wrap">
+                                <i className={` ${"icon-Vector-32"}`} />
+                                {data.value}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-12">
+                  <div className="sticky-section">
+                    <div className="driver-price-wrap mb-40">
+                      <div className="box-2 t-al-right">
+                        <div className="flex align-center gp-15">
+                          <p className="price-sale">$ {product.price}</p>
+                        </div>
+                        <p className="converted-price mt-15">
+                          <h5 class="price-another-currency">
+                            <div class="second-currency-price">
+                              {" "}
+                              ≈ €40,000 &nbsp; ≈ ₼80,000
+                            </div>
+                          </h5>
+                        </p>
+                        <div className="ad-details">
+                          <div>
+                            <CalendarMonthIcon /> 07.08.2024
+                          </div>
+                          <div>
+                            <VisibilityIcon /> 2000
+                          </div>
+                        </div>
+                      </div>
+                      <div className="author-contact-wrap">
+                        <div className="author-contact-avatar">
+                          <AccountCircleIcon />
+                        </div>
+                        <div className="author-contact-info">
+                          <h4 className="name"> Admin </h4>
+                          {/* <span className="number-phone">
+                          <i className=" icon-Group-14" />
+                          +1900 67 xxx (show)
+                        </span> */}
+                        </div>
+                      </div>
+                      <div className="ad-owner-details">
+                        <p>Contact Details:</p>
+                        <div className="ad-owner-contact-number">
+                          <i className=" icon-Group-14" /> +1900 67 xxx (show)
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* property-detail */}
+        {/* related-single-listing */}
+        <div className="widget-related-single-listing">
+          <div className="themesflat-container">
+            <div className="ad-banner">
+              <img src="/assets/images/ads/ad-banner.jpg" />
+            </div>
+            <div className="divider-header">Related Ads</div>
+            <div className="car-list-item">
+              {relatedAds.length > 0 &&
+                relatedAds?.map((ad) => {
+                  return <AdCard key={ad.id} data={ad} />;
+                })}
+              <button>See All Ads</button>
+            </div>
+            <div className="ad-banner">
+              <img src="/assets/images/ads/ad-banner.jpg" />
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
